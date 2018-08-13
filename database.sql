46bb7dd3a625f19590149d2519c8f01d90487eb6
Grace Kasahara <gracekasahara@gmail.com>
2018-08-13 14:02:42 -0500

    added files for each needed component

--- database.sql
+++ database.sql
@@ -1,5 +1,27 @@
-CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL
+CREATE TABLE "person" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "role" VARCHAR (80) NOT NULL
+);
+
+CREATE TABLE "therapist_info" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT,
+    "first_name" VARCHAR (1000),
+    "last_name" VARCHAR (1000)
+);
+
+CREATE TABLE "patient_info" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT,
+	"therapist_id" INT
+);
+
+CREATE TABLE "daily_log" (
+    "id" SERIAL PRIMARY KEY,
+    "depression_rating" INT NOT NULL,
+    "activity" VARCHAR (1000) NOT NULL,
+    "date_time" DATE,
+    "patient_id" INT
 );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = ({ title }) => (
   <div className="instructions">
     <div>
-      <h1 className="lead">{ title }</h1>
+      <h1 className="lead">Depression Activity Chart</h1>
+      {/* <h1 className="lead">{ title }</h1> */}
     </div>
   </div>
 );
