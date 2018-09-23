9c23e4be4f6a33654cfa15383093c545ddef5cce
Grace Kasahara <gracekasahara@gmail.com>
2018-09-23 11:42:54 -0500

    added a dropdown to the patient log

--- src/components/PatientLog/PatientLog.js
+++ src/components/PatientLog/PatientLog.js
@@ -7,11 +7,34 @@ import PatientChooseTherapist from '../PatientChooseTherapist/PatientChooseThera
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+
 
 const mapStateToProps = state => ({
     user: state.user,
     therapistName: state.therapistName
 });
+
+const styles = {
+    textField: {
+        margin: '0em 0em 0.5em 0em',
+        width: "14em"
+    },
+    dropDown: {
+        width: "10em"
+    },
+    formControl: {
+        margin: '0em 0em 0.5em 0em',
+        width: "5em"
+    },
+    selectEmpty: {
+        marginTop: '0em 0em 0.5em 0em' * 2,
+    }
+}
 
 class PatientLog extends Component {
 
@@ -26,13 +49,13 @@ class PatientLog extends Component {
         }
     }
 
-    
+
 
     componentDidMount() {
         this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
         this.props.dispatch({ type: 'GET_CURRENT_THERAPIST' });
         console.log('role', this.props.user.role)
-        if (this.props.user.role === 'therapist'){
+        if (this.props.user.role === 'therapist') {
             this.props.history.push('therapistHome');
         }
     }
@@ -59,22 +82,20 @@ class PatientLog extends Component {
         }
     }
 
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    };
+
     addLog = () => {
         this.props.dispatch({
             type: 'POST_LOG',
             payload: this.state.newLog
         })
-        // this.setState({
-        //     newLog: {
-        //         depression_rating: '',
-        //         activity: ''
-        //     }
-        // });
         this.props.history.push('patientGraph');
     }
 
     render() {
-        
+
         let content = null;
         if (this.props.user.userName && this.props.user.role === 'patient') {
             if (this.props.therapistName.first_name === undefined) {
@@ -83,35 +104,78 @@ class PatientLog extends Component {
                         <PatientChooseTherapist />
                     </div>
             } else {
-            content = (
-                <div>
-                    <Grid container justify="center" id="welcome">
-                    <Grid item xs={12}>
-                    <Nav />
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={8}>
-                    <h2>Record Your Mood</h2>
-                    </Grid>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={12}>
-                    <p>Rate the severity of your depressed mood from 0 (none) to 10 (severe) </p>
-                    </Grid>
-                    <Grid item xs={4}></Grid>
-                    <Grid item xs={4}>
-                    <input placeholder="Depressed Mood Rating" onChange={this.handleChangeFor("depression_rating")} />
-                    </Grid>
-                    <Grid item xs={4}></Grid>
-                    <Grid item xs={12}>
-                    <p>What are you doing right now?</p>
-                    </Grid>
-                    <input placeholder="Current Activity" onChange={this.handleChangeFor("activity")} />
-                    <Button variant="raised" onClick={this.addLog}>Submit</Button>
-                    {/* <Button variant="raised" onClick={this.home}>Back</Button> */}
-                    </Grid>
-                </div>
-            );
-        }} 
+                content = (
+                    <div>
+                        <Grid container justify="center" id="welcome">
+                            <Grid item xs={12}>
+                                <Nav />
+                            </Grid>
+                            <Grid item xs={2}></Grid>
+                            <Grid item xs={8}>
+                                <h2>Record Your Mood</h2>
+                            </Grid>
+                            <Grid item xs={2}></Grid>
+                            <Grid item xs={12}>
+                                <p>Rate the severity of your depressed mood from 0 (none) to 10 (severe) </p>
+                            </Grid>
+                            <Grid item xs={4}></Grid>
+                            <Grid item xs={4}>
+                                {/* <input placeholder="Depressed Mood Rating" onChange={this.handleChangeFor("depression_rating")} /> */}
+                            </Grid>
+                            <Grid item xs={4}></Grid>
+                            <FormControl className={this.props.classes.formControl}>
+                                <InputLabel>Rating</InputLabel>
+                                <Select
+                                value={this.state.newLog.depression_rating}
+                                onChange={this.handleChangeFor("depression_rating")}
+                                // id='SLODrop'
+                                >
+                                    <MenuItem value="0">
+                                        <em>0</em>
+                                    </MenuItem>
+                                    <MenuItem value="1">
+                                        <em>1</em>
+                                    </MenuItem>
+                                    <MenuItem value="2">
+                                        <em>2</em>
+                                    </MenuItem>
+                                    <MenuItem value="3">
+                                        <em>3</em>
+                                    </MenuItem>
+                                    <MenuItem value="4">
+                                        <em>4</em>
+                                    </MenuItem>
+                                    <MenuItem value="5">
+                                        <em>5</em>
+                                    </MenuItem>
+                                    <MenuItem value="6">
+                                        <em>6</em>
+                                    </MenuItem>
+                                    <MenuItem value="7">
+                                        <em>7</em>
+                                    </MenuItem>
+                                    <MenuItem value="8">
+                                        <em>8</em>
+                                    </MenuItem>
+                                    <MenuItem value="9">
+                                        <em>9</em>
+                                    </MenuItem>
+                                    <MenuItem value="10">
+                                        <em>10</em>
+                                    </MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Grid item xs={12}>
+                                <p>What are you doing right now?</p>
+                            </Grid>
+                            <input placeholder="Current Activity" onChange={this.handleChangeFor("activity")} />
+                            <Button variant="raised" onClick={this.addLog}>Submit</Button>
+                            {/* <Button variant="raised" onClick={this.home}>Back</Button> */}
+                        </Grid>
+                    </div>
+                );
+            }
+        }
 
         return (
             <div>
@@ -121,4 +185,5 @@ class PatientLog extends Component {
     }
 }
 
-export default connect(mapStateToProps)(PatientLog);
+const styleLog = withStyles(styles)(PatientLog)
+export default connect(mapStateToProps)(styleLog);
